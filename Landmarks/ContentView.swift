09e14395e8808aad0d7cf418a5110b5d5ff1e9ee
Vincent Frascello <vincent.frascello@gmail.com>
2020-12-31 17:31:17 -0800

    Created Complete View per Landmark

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -3,19 +3,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading)
-        {
-            Text("Shilling Rock")
-                .font(.title)
-    
-            HStack {
-                Text("Brenden Shilling National Historic Site")
-                    .font(.subheadline)
-                Spacer()
-                Text("Ohio")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading)
+            {
+                Text("Shilling Rock")
+                    .font(.title)
+        
+                HStack {
+                    Text("Brenden Shilling National Historic Site")
+                      
+                    Spacer()
+                    Text("Ohio")
+                        
+                }.font(.subheadline)
+                .foregroundColor(.secondary)
+            Divider()
+                Text("About Shilling Rock")
+                    .font(.title2)
+                Text("It was under this rock where Lisa Shilling gave birth to Brenden on a cold winter night in 1988.")
             }
-        }.padding(10)
+            .padding(10)
+            Spacer()
+        }
     }
 }
 
